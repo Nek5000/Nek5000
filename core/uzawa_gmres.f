50f16e6dfe48afd1b6caf039082cdea48fc3639c
stefanke <stefanke@24248562-e74d-0410-ab4b-d4d80c744c73>
2008-11-19 19:54:37 +0000

    removed some comment statement
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/nek@134 24248562-e74d-0410-ab4b-d4d80c744c73

--- core/uzawa_gmres.f
+++ core/uzawa_gmres.f
@@ -56,7 +56,7 @@ c
 c
       iconv = 0
       call rzero(x,ntot2)
-      do while(iconv.eq.0.and.iter.lt.200)
+      do while(iconv.eq.0.and.iter.lt.500)
          if(iter.eq.0) then
                                                   !      -1
             call col3(r,ml,res,ntot2)             ! r = L  res
@@ -335,7 +335,7 @@ c     GMRES iteration.
 c
       iconv = 0
       call rzero(x,n)
-      do while(iconv.eq.0.and.iter.lt.200)
+      do while(iconv.eq.0.and.iter.lt.500)
          if(iter.eq.0) then               !      -1
             call col3(r,ml,res,n)         ! r = L  res
 c           call copy(r,res,n)
