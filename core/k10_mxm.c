7f8a00c871f468fd1fd53e1ca9617bc1dace721a
stefanke <stefanke@24248562-e74d-0410-ab4b-d4d80c744c73>
2009-10-27 09:40:52 +0000

    added tuned K10_MXM for lx1=10
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/nek@354 24248562-e74d-0410-ab4b-d4d80c744c73

--- core/k10_mxm.c
+++ core/k10_mxm.c
@@ -15,9 +15,13 @@
 
 #define k10_mxm FORTRAN_NAME(k10_mxm, K10_MXM)
 
-void tune_mxm888 (double*, double*, double*);
-void tune_mxm8864(double*, double*, double*);
-void tune_mxm6488(double*, double*, double*);
+void tune_mxm888    (double*, double*, double*);
+void tune_mxm8864   (double*, double*, double*);
+void tune_mxm6488   (double*, double*, double*);
+void tune_mxm101010 (double*, double*, double*);
+void tune_mxm1010100(double*, double*, double*);
+void tune_mxm1001010(double*, double*, double*);
+
 
 int k10_mxm(double* a, int* sz1, double* b, int* sz2, double* c, int* sz3)
 {
@@ -38,5 +42,15 @@ int k10_mxm(double* a, int* sz1, double* b, int* sz2, double* c, int* sz3)
        }
        else if (m == 64 && n == 8){tune_mxm6488(a,b,c); return 0;}
     }
+
+    // 10,10,10  10,10,100  100,10,10
+    if (k == 10){
+       if (m == 10){
+          if     (n ==  10){tune_mxm101010 (a,b,c); return 0;}
+          else if(n == 100){tune_mxm1010100(a,b,c); return 0;}
+       }
+       else if (m == 100 && n == 10){tune_mxm1001010(a,b,c); return 0;}
+    }
+
     return 1;
 }
