d6cf756a958df3e0930f847866c8e1e4cefac55e
Katherine Heisey <klheisey@gmail.com>
2011-03-18 15:52:40 +0000

    took out redundant integer declaration...again
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/tools@669 24248562-e74d-0410-ab4b-d4d80c744c73

--- tools/prenek/curve.f
+++ tools/prenek/curve.f
@@ -1255,7 +1255,6 @@ c-----------------------------------------------------------------------
 c     Assign / repair curve-side info for edges
 
       include 'basics.inc'
-      integer e
 
       real xyz(3,3)
 
