99547e31aec8854de48488d563b2b5e772565275
Stefan Kerkemeier <stgeke@gmail.com>
2011-11-09 14:56:16 +0000

    moving mesh support for incompressible PnPn
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/nek@730 24248562-e74d-0410-ab4b-d4d80c744c73

--- core/qthermal.f
+++ core/qthermal.f
@@ -21,39 +21,34 @@ C     energy equation expressed in terms of temperature.
      $              ,ty(LX1,LY1,LZ1,LELT)
      $              ,tz(LX1,LY1,LZ1,LELT)
 
-
       ntot = nx1*ny1*nz1*nelv
 
-      ifld_save = ifield
-
       if (.not.iflomach) then
-          call rzero(qtl,ntot)
+         call rzero(qtl,ntot)
          return
       endif
 
+      ifld_save = ifield
+
 c - - Assemble RHS of T-eqn
-      ! volumetric heating source
       ifield=2
-      call setqvol (QTL)
+      call setqvol (QTL) ! volumetric heating source
       call col2    (QTL,BM1,ntot)
 
+      ifield=1     !set right gs handle (QTL is only defined on the velocity mesh)
       call opgrad  (tx,ty,tz,T)
       call opdssum (tx,ty,tz)
       call opcolv  (tx,ty,tz,binvm1)
-
-      ! diffusion term
       call opcolv  (tx,ty,tz,vdiff(1,1,1,1,2))
       call opdiv   (w2,tx,ty,tz)
 
-      CALL ADD2    (QTL,w2,ntot)
+      call add2    (QTL,w2,ntot)
 
-      ifield=1     !set right gs handle (QTL is only defined on the velocity mesh)
-      call dssum   (QTL,nx1,ny1,nz1)
-      call col2    (QTL,binvm1,ntot)
-
-      ! QTL = T_RHS/(rho*cp**T)
       call col3    (w2,vtrans(1,1,1,1,2),T,ntot)
       call invcol2 (QTL,w2,ntot)
+
+      call dssum   (QTL,nx1,ny1,nz1)
+      call col2    (QTL,binvm1,ntot)
 
       ifield = ifld_save
 
