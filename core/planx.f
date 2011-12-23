70c9126ef8b5061fd25c6958b4855be32c06467c
fischer <fischer@24248562-e74d-0410-ab4b-d4d80c744c73>
2011-12-23 04:12:46 +0000

    fixed velocity projection issue
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/nek@748 24248562-e74d-0410-ab4b-d4d80c744c73

--- core/planx.f
+++ core/planx.f
@@ -5,11 +5,11 @@ C     Compute pressure and velocity using consistent approximation spaces.
 C     Operator splitting technique.
 C
 C-----------------------------------------------------------------------
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'EIGEN'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
+      include 'SIZE'
+      include 'INPUT'
+      include 'EIGEN'
+      include 'SOLN'
+      include 'TSTEP'
 C
       COMMON /SCRNS/  RESV1 (LX1,LY1,LZ1,LELV)
      $ ,              RESV2 (LX1,LY1,LZ1,LELV)
@@ -64,9 +64,9 @@ C
 C     Keep old pressure values
 C
 C--------------------------------------------------------------------
-      INCLUDE 'SIZE'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
+      include 'SIZE'
+      include 'SOLN'
+      include 'TSTEP'
 
       common /cgeom/ igeom
 
@@ -83,8 +83,8 @@ C
 C     Compute startresidual/right-hand-side in the velocity solver
 C
 C---------------------------------------------------------------------
-      INCLUDE 'SIZE'
-      INCLUDE 'TOTAL'
+      include 'SIZE'
+      include 'TOTAL'
       REAL           RESV1 (LX1,LY1,LZ1,1)
       REAL           RESV2 (LX1,LY1,LZ1,1)
       REAL           RESV3 (LX1,LY1,LZ1,1)
@@ -117,9 +117,9 @@ C
 C     Pressure extrapolation
 C
 C--------------------------------------------------------------------
-      INCLUDE 'SIZE'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
+      include 'SIZE'
+      include 'SOLN'
+      include 'TSTEP'
       COMMON /CTMP0/ DPR (LX2,LY2,LZ2,LELV)
       REAL        PREXTR (LX2,LY2,LZ2,LELV)
 
@@ -146,11 +146,11 @@ c-----------------------------------------------------------------------
 C
 C     OT = (H1*A+H2*B)-1 * IN  (implicit)
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'ORTHOV'
-      INCLUDE 'TSTEP'
-      INCLUDE 'SOLN'
+      include 'SIZE'
+      include 'INPUT'
+      include 'ORTHOV'
+      include 'TSTEP'
+      include 'SOLN'
 c
       REAL OT1 (LX1,LY1,LZ1,1)
       REAL OT2 (LX1,LY1,LZ1,1)
@@ -183,9 +183,9 @@ C
       end
 c-----------------------------------------------------------------------
       subroutine hmzpf2(nm,u,rhs,h1,h2,mask,mult,imsh,tol,mxit,isd)
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
       character*4 nm
 c
       REAL           U    (LX1,LY1,LZ1,1)
@@ -194,267 +194,271 @@ c
       REAL           H2   (LX1,LY1,LZ1,1)
       REAL           MASK (LX1,LY1,LZ1,1)
       REAL           MULT (LX1,LY1,LZ1,1)
-C
+
       ntot1 = nx1*ny1*nz1*nelv
       if (imsh.eq.2) ntot1 = nx1*ny1*nz1*nelt
-c
+
       call col2   (rhs,mask,ntot1)
       call dssum  (rhs,nx1,ny1,nz1)
-      call projh2 (rhs,h1,h2,mult,mask,isd)
-      call hmhzpf (nm,u,rhs,h1,h2,mask,mult,imsh,tol,mxit,isd,binvm1)
-      call gensh2 (u,h1,h2,mult,mask,isd)
-c
+      call projh2 (rhs,h1,h2,mult,mask,isd,imsh)
+      if (imsh.eq.1) then
+        call hmhzpf (nm,u,rhs,h1,h2,mask,mult,imsh,tol,mxit,isd,binvm1)
+      else
+        call hmhzpf (nm,u,rhs,h1,h2,mask,mult,imsh,tol,mxit,isd,bintm1)
+      endif
+      call gensh2 (u,h1,h2,mult,mask,isd,imsh)
+
       return
       end
 c-----------------------------------------------------------------------
-      subroutine projh2(v1,h1,h2,vml,vmask,isd)
+      subroutine projh2(v1,h1,h2,vml,vmask,isd,imsh)
 C
 C     Orthogonalize the rhs wrt previous rhs's for which we already
 C     know the soln.
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
-      INCLUDE 'ORTHOV'
-C
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'SOLN'
+      include 'TSTEP'
+      include 'ORTHOV'
+
       real v1(1),h1(1),h2(1),vml(1),vmask(1)
       real work(mxprev)
-C
+
       integer icalld
       save    icalld
       data    icalld/0/
-C
+
       ntot1=nx1*ny1*nz1*nelv
-      n1 = 1
-      if (icalld.eq.0) then
-C        First call, we have no vectors to orthogonalize against.
-         do j=1,ndim
-            Nprev(j)=0
-         enddo
-         Mprev=param(93)
-         Mprev=min(Mprev,Mxprev)
+      if (imsh.eq.2) ntot1=nx1*ny1*nz1*nelt
+
+      if (icalld.eq.0) then ! First call, no vectors to orthogonalize against.
+         call izero(nprev,ndim)
+         mprev=param(93)
+         mprev=min(mprev,mxprev)
          if (mprev.eq.0) mprev = mxprev
          if (nid.eq.0) write(6,*) 'this is mprev:',mprev,mxprev
       endif
-C
+
 C     Diag to see how much reduction in the residual is attained.
-C
-      ALPHA1 = glsc23(v1,binvm1,vml,ntot1)
-      ALPHA1 = sqrt(alpha1/volvm1)
-      if (icalld.eq.0.and.nid.eq.0) 
-     $     write(6,*) 'alpha1:',alpha1,volvm1,ntot1
-      if (icalld.eq.0.and.nid.eq.0) 
-     $     write(6,*) 'binvm1:',binvm1(1,1,1,1),vml(1),v1(1)
-C
-C     Update rhs's if matrix has changed
-C
-      call updrhsh2(h1,h2,vml,vmask,isd)
-C
-C     Perform Gram-Schmidt for previous soln's
-C
-      call rzero(alpha,mxprev)
+      if (imsh.eq.1) then
+         alpha1 = glsc23(v1,binvm1,vml,ntot1)
+         alpha1 = sqrt(alpha1/volvm1)
+      else
+         alpha1 = glsc23(v1,bintm1,vml,ntot1)
+         alpha1 = sqrt(alpha1/voltm1)
+      endif
+
+c     if (icalld.eq.0.and.nid.eq.0) 
+c    $     write(6,*) 'alpha1:',alpha1,volvm1,ntot1
+c     if (icalld.eq.0.and.nid.eq.0) 
+c    $     write(6,*) 'binvm1:',binvm1(1,1,1,1),vml(1),v1(1)
+
+
+      call updrhsh2(h1,h2,vml,vmask,isd,imsh) ! Update rhs's if matrix has changed
+
+
+      call rzero(alpha,mxprev) !  Gram-Schmidt for previous soln's
       ioff = 1
-      do i=1,Nprev(isd)
-         ALPHA(i) = vlsc3(v1,sln(ioff,isd),vml,NTOT1)
+      do i=1,nprev(isd)
+         alpha(i) = vlsc3(v1,sln(ioff,isd),vml,ntot1)
          ioff = ioff + ntot1
       enddo
-      IF (Nprev(isd).GT.0) then
-         call gop(alpha,WORK,'+  ',Nprev(isd))
-         call cmult2(vbar(n1,isd),sln(1,isd),alpha(1),ntot1)
-C
-         do i=2,Nprev(isd)
+
+      if (nprev(isd).gt.0) then
+         call gop(alpha,work,'+  ',nprev(isd))
+         call cmult2(vbar(1,isd),sln(1,isd),alpha(1),ntot1)
+
+         do i=2,nprev(isd)
             ioff = ntot1*(i-1)+1
-            call add2s2(vbar(n1,isd),sln(ioff,isd),alpha(i),ntot1)
+            call add2s2 (vbar(1,isd),sln(ioff,isd),alpha(i),ntot1)
          enddo
-c        alphmn = glmin(vbar(n1,isd),ntot1)
-c        alphmx = glmax(vbar(n1,isd),ntot1)
-         call axhelm  (vnew(n1,isd),vbar(n1,isd),H1,H2,1,1)
-c        alp1mn = glmin(vnew(n1,isd),ntot1)
-c        alp1mx = glmax(vnew(n1,isd),ntot1)
-         call col2    (vnew(n1,isd),vmask,ntot1)
-c        alp2mn = glmin(vnew(n1,isd),ntot1)
-c        alp2mx = glmax(vnew(n1,isd),ntot1)
-         call dssum   (vnew(n1,isd),nx1,ny1,nz1)
-c        alp3mn = glmin(vnew(n1,isd),ntot1)
-c        alp3mx = glmax(vnew(n1,isd),ntot1)
-         call sub2    (v1,vnew(n1,isd),ntot1)
+c        alphmn = glmin (vbar(1,isd),ntot1)
+c        alphmx = glmax (vbar(1,isd),ntot1)
+         call axhelm    (vnew(1,isd),vbar(1,isd),H1,H2,1,1)
+c        alp1mn = glmin (vnew(1,isd),ntot1)
+c        alp1mx = glmax (vnew(1,isd),ntot1)
+         call col2      (vnew(1,isd),vmask,ntot1)
+c        alp2mn = glmin (vnew(1,isd),ntot1)
+c        alp2mx = glmax (vnew(1,isd),ntot1)
+         call dssum     (vnew(1,isd),nx1,ny1,nz1)
+c        alp3mn = glmin (vnew(1,isd),ntot1)
+c        alp3mx = glmax (vnew(1,isd),ntot1)
+         call sub2      (v1,vnew(1,isd),ntot1)
+      else
+         call rzero     (vnew(1,isd),ntot1)
+         call rzero     (vbar(1,isd),ntot1)
       endif
+
 c     if (nid.eq.0) write(6,90) istep,alphmn,alphmx
 c    $              ,alp1mn,alp1mx,alp2mn,alp2mx,alp3mn,alp3mx
-   90 format(i4,1p8e11.3,' xx')
-C ................................................................
-C     Diag.
-      ALPHA2 = glsc23(v1,binvm1,vml,ntot1)
-      ALPHA2 = sqrt(alpha2/volvm1)
-      alphmn = glmax(v1,ntot1)
-      alphmx = glmin(v1,ntot1)
+c  90 format(i4,1p8e11.3,' xx')
+
+C     Diag. ............................................................
+      if (imsh.eq.1) then
+         alpha2 = glsc23(v1,binvm1,vml,ntot1)
+         alpha2 = sqrt(alpha2/volvm1)
+      else
+         alpha2 = glsc23(v1,bintm1,vml,ntot1)
+         alpha2 = sqrt(alpha2/voltm1)
+      endif
       ratio  = alpha1/alpha2
-      n10=min(10,Nprev(isd))
-      if (nid.eq.0) write(6,10) istep,alpha1,alpha2,ratio,Nprev(isd)
-c     if (nid.eq.0) write(6,10) istep,alphmn,alphmx,ratio,Nprev(isd)
-   10 FORMAT(I6,1p3e12.4,i6,' alph1x')
-      IF (NID.EQ.0) WRITE(6,11) ISTEP,Nprev(isd),(ALPHA(I),I=1,n10)
-   11 FORMAT(I6,' halpha',i4,10(1p10e12.4,/,17x))
-C     Diag.
-C ................................................................
-c
+      n10=min(10,nprev(isd))
+      if (nid.eq.0) write(6,10) istep,alpha1,alpha2,ratio,nprev(isd)
+   10 format(i6,1p3e12.4,i6,' alph1x')
+      if (nid.eq.0) write(6,11) istep,nprev(isd),(alpha(I),I=1,n10)
+   11 format(i6,' halpha',i4,10(1p10e12.4,/,17x))
+
+c     alphmn = glmax(v1,ntot1)
+c     alphmx = glmin(v1,ntot1)
+c     if (nid.eq.0) write(6,10) istep,alphmn,alphmx,ratio,nprev(isd)
+
+C     Diag. .............................................................
+
       icalld=icalld+1
+
       return
       end
 c-----------------------------------------------------------------------
-      subroutine gensh2(v1,h1,h2,vml,vmask,isd)
+      subroutine gensh2(v1,h1,h2,vml,vmask,isd,imsh)
 C
 C     Reconstruct the solution to the original problem by adding back
 C     the previous solutions
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
-      INCLUDE 'ORTHOV'
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'SOLN'
+      include 'TSTEP'
+      include 'ORTHOV'
       real v1(1),h1(1),h2(1),vml(1),vmask(1)
-C
+
       ntot1=nx1*ny1*nz1*nelv
-      n1 = 1
-C
-C     First, save current solution
-C
-      call copy (vnew(n1,isd),v1,ntot1)
-C
-C     Reconstruct solution
-C
-      call add2(v1,vbar(n1,isd),ntot1)
-C
-C     Update the set of <sln>
-C
-      call updtseth2(v1,h1,h2,vml,vmask,isd)
-C
+      if (imsh.eq.2) ntot1=nx1*ny1*nz1*nelt
+
+      call copy (vnew(1,isd),v1,ntot1)            !  Save current solution
+      call add2(v1,vbar(1,isd),ntot1)             !  Reconstruct solution
+      call updtseth2(v1,h1,h2,vml,vmask,isd,imsh) !  Update {SLN}
+
       return
       end
 c-----------------------------------------------------------------------
-      subroutine updtseth2(v1,h1,h2,vml,vmask,isd)
+      subroutine updtseth2(v1,h1,h2,vml,vmask,isd,imsh)
 C
 C     Update the set of rhs's and the corresponding p-set:
 C
 C        . Standard case is to add P_new, and RHS_new = E*P_new
 C
-C        . However, when Nprev=Mprev (max. allowed), we throw out
+C        . However, when nprev=mprev (max. allowed), we throw out
 C          the old set, and set P_1 = P, RHS_1=E*P_1
 C
 C        . Other schemes are possible, e.g., let's save a bunch of
 C          old vectors, perhaps chosen wisely via P.O.D.
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
-      INCLUDE 'ORTHOV'
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'SOLN'
+      include 'TSTEP'
+      include 'ORTHOV'
       real v1(1),h1(1),h2(1),vml(1),vmask(1)
-C
+
       ntot1=nx1*ny1*nz1*nelv
-      n1 = 1
-C     
-      IF (Nprev(isd).EQ.Mprev) THEN
-         CALL COPY(vnew(n1,isd),v1,ntot1)
-         Nprev(isd)=0
-      ENDIF
-C
-C     Increment solution set
-      Nprev(isd) = Nprev(isd)+1
-      ioff = ntot1*(Nprev(isd)-1)+1
-      CALL COPY(sln(ioff,isd),vnew(n1,isd),NTOT1)
-c
-C     Orthogonalize rhs against previous rhs and normalize
-      call hconj2(Nprev(isd),H1,H2,vml,vmask,isd)
-c
+      if (imsh.eq.2) ntot1=nx1*ny1*nz1*nelt
+     
+      if (nprev(isd).eq.mprev) then
+         call copy(vnew(1,isd),v1,ntot1)
+         nprev(isd)=0
+      endif
+
+c     Increment solution set
+      nprev(isd) = nprev(isd)+1
+      ioff = ntot1*(nprev(isd)-1)+1
+      call copy(sln(ioff,isd),vnew(1,isd),ntot1)
+
+c     Orthogonalize rhs against previous rhs and normalize
+      call hconj2(nprev(isd),h1,h2,vml,vmask,isd,imsh)
+
 c     Save last sol'n
-      CALL COPY(vnew(n1,isd),v1,ntot1)
-c
+      call copy(vnew(1,isd),v1,ntot1)
+
       return
       end
 c-----------------------------------------------------------------------
-      subroutine hconj2(kprev,h1,h2,vml,vmask,isd)
+      subroutine hconj2(kprev,h1,h2,vml,vmask,isd,imsh)
 C
 C     Orthonormalize the last saved vector against vector set
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
-      INCLUDE 'SOLN'
-      INCLUDE 'TSTEP'
-      INCLUDE 'ORTHOV'
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'SOLN'
+      include 'TSTEP'
+      include 'ORTHOV'
       real h1(1),h2(1),vml(1),vmask(1)
       real work(mxprev)
-C
-      ntot1=nx1*ny1*nz1*nelv
-      n1 = 1
-C
-      i1 = (kprev-1)*ntot1 + 1
-      call axhelm  (vbar(n1,isd),sln(i1,isd),H1,H2,1,1)
-      call col2    (vbar(n1,isd),vmask,ntot1)
-      call dssum   (vbar(n1,isd),nx1,ny1,nz1)
-C
-C     Compute part of the norm
-C
-      call col2     (vbar(1,isd),vml   ,ntot1)
-      alphad = glsc2(vbar(1,isd),sln(i1,isd),ntot1)
-C
-C     Gram-Schmidt
-      Kprev1=Kprev-1
-      DO 10 I=1,Kprev1
+
+      ntot1 = nx1*ny1*nz1*nelv
+      if (imsh.eq.2) ntot1 = nx1*ny1*nz1*nelt
+
+      kprev1 = kprev-1
+      i1     = kprev*ntot1 + 1
+
+      call axhelm  (vbar(1,isd),sln(i1,isd),h1,h2,1,1)
+      call col2    (vbar(1,isd),vmask,ntot1)
+      call dssum   (vbar(1,isd),nx1,ny1,nz1)
+      call col2    (vbar(1,isd),vml   ,ntot1) ! Compute part of the norm
+      alphad=glsc2 (vbar(1,isd),sln(i1,isd),ntot1)
+
+      do i=1,kprev1                    ! Gram-Schmidt
          ioff = (i-1)*ntot1 + 1
-         ALPHA(I) = VLSC2(vbar(1,isd),sln(ioff,isd),ntot1)
-   10 CONTINUE
-      IF (Kprev1.GT.0) call gop(alpha,WORK,'+  ',Kprev1)
-C
-      DO 20 I=1,Kprev1
+         alpha(i) = vlsc2(vbar(1,isd),sln(ioff,isd),ntot1)
+      enddo
+      if (kprev1.gt.0) call gop(alpha,work,'+  ',kprev1)
+
+      do i=1,kprev1
          alpham = -alpha(i)
          ioff = (i-1)*ntot1 + 1
-         CALL ADD2S2(sln(i1,isd),sln(ioff,isd),alpham,ntot1)
-         Alphad = Alphad - alpha(i)**2
-   20 CONTINUE
-C
-C    .Normalize new element in P~
-C
-      ALPHAd = 1.0/SQRT(ALPHAd)
-      CALL CMULT(sln(i1,isd),alphad,ntot1)
-C
+         call add2s2(sln(i1,isd),sln(ioff,isd),alpham,ntot1)
+         alphad = alphad - alpha(i)**2
+      enddo
+
+c    .Normalize new element in P~
+      alphad = 1.0/sqrt(alphad)
+      call cmult(sln(i1,isd),alphad,ntot1)
+
       return
       end
 c-----------------------------------------------------------------------
-      subroutine updrhsh2(h1,h2,vml,vmask,isd)
+      subroutine updrhsh2(h1,h2,vml,vmask,isd,imsh)
 C
 C     Update rhs's if A-matrix has changed
 C
-      INCLUDE 'SIZE'
-      INCLUDE 'INPUT'
-      INCLUDE 'MASS'
-      INCLUDE 'ORTHOV'
-      INCLUDE 'TSTEP'
-C
-C
-      REAL vml(1),h1(1),h2(1),vmask(1)
-C
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'ORTHOV'
+      include 'TSTEP'
+
+
+      real vml(1),h1(1),h2(1),vmask(1)
+
       real    dtold
       save    dtold
       data    dtold/0.0/
-c
+
 C     First, we have to decide if the E matrix has changed.
-C
+
       if (dt.eq.dtold) return
       dtold = dt
       nprev(isd) = 0
       return
-c
-      DO 100 Iprev=1,Nprev(isd)
-C        Orthogonalize this rhs w.r.t. previous rhs's
-         call hconj2(Iprev,H1,H2,vml,vmask,isd)
-  100 CONTINUE
-C
+
+      do iprev=1,nprev(isd) ! Orthogonalize this rhs w.r.t. previous rhs's
+         call hconj2(iprev,h1,h2,vml,vmask,isd,imsh)
+      enddo
+
       return
       end
 c-----------------------------------------------------------------------
