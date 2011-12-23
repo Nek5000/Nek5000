b693f5ddc0473dd982decdc88d3a4e682e5985a3
Tim Tautges <tautges@mcs.anl.gov>
2011-12-23 19:31:43 +0000

    Fixing a few problems noted by Katie.
    nek/3rd_party/moab.f: removing #ifdef'd out code; setting tag sizes according to
      e.g. nx1, not lx1
    nek/3rd_party/imeshcutil.c: inserted #ifdef MPI for serial build
    nek/3rd_party/NEKMOAB: setting POINTER_SIZE according to Nek PTRSIZE8 make variable
    
    
    
    git-svn-id: https://svn.mcs.anl.gov/repos/nek5/trunk/nek@751 24248562-e74d-0410-ab4b-d4d80c744c73

--- core/3rd_party/imeshcutil.c
+++ core/3rd_party/imeshcutil.c
@@ -1,7 +1,13 @@
 /* I can't believe I need a function to do this... */
+#ifdef MPI
 #include "mpi.h"
+#else
+#define MPI_Comm int
+#endif
 
 void moab_comm_f2c_(int *fcomm, MPI_Comm *ccomm) 
 {
+#ifdef MPI  
   *ccomm = MPI_Comm_f2c(*fcomm);
+#endif
 }
