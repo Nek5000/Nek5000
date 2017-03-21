da312a655b48b4082969c0bdab8d37bda78e9baa
Stefan <stgeke@gmail.com>
2017-03-21 10:07:42 +0100

    make IFAMG a runtime parameter, link jl as a library

--- jl/crs.h
+++ jl/crs.h
@@ -5,20 +5,32 @@
 #warning "crs.h" requires "comm.h"
 #endif
 
-#define crs_setup PREFIXED_NAME(crs_setup)
-#define crs_solve PREFIXED_NAME(crs_solve)
-#define crs_stats PREFIXED_NAME(crs_stats)
-#define crs_free  PREFIXED_NAME(crs_free )
+#define crs_xxt_setup PREFIXED_NAME(crs_xxt_setup)
+#define crs_xxt_solve PREFIXED_NAME(crs_xxt_solve)
+#define crs_xxt_stats PREFIXED_NAME(crs_xxt_stats)
+#define crs_xxt_free  PREFIXED_NAME(crs_xxt_free )
+
+#define crs_amg_setup PREFIXED_NAME(crs_amg_setup)
+#define crs_amg_solve PREFIXED_NAME(crs_amg_solve)
+#define crs_amg_stats PREFIXED_NAME(crs_amg_stats)
+#define crs_amg_free  PREFIXED_NAME(crs_amg_free )
 
 struct crs_data;
 
-struct crs_data *crs_setup(
+struct crs_data *crs_xxt_setup(
   uint n, const ulong *id,
   uint nz, const uint *Ai, const uint *Aj, const double *A,
   uint null_space, const struct comm *comm);
-void crs_solve(double *x, struct crs_data *data, double *b);
-void crs_stats(struct crs_data *data);
-void crs_free(struct crs_data *data);
+void crs_xxt_solve(double *x, struct crs_data *data, double *b);
+void crs_xxt_stats(struct crs_data *data);
+void crs_xxt_free(struct crs_data *data);
+
+struct crs_data *crs_amg_setup(
+  uint n, const ulong *id,
+  uint nz, const uint *Ai, const uint *Aj, const double *A,
+  uint null_space, const struct comm *comm);
+void crs_amg_solve(double *x, struct crs_data *data, double *b);
+void crs_amg_stats(struct crs_data *data);
+void crs_amg_free(struct crs_data *data);
 
 #endif
-
