ab1bf007d7f5558f10abcacdc2c71bc8b2a4b220
Stefan <stgeke@gmail.com>
2017-04-23 13:01:05 -0500

    remove old amg setup tool

--- tools/amg_matlab/amg_gamma.m
+++
@@ -1,8 +0,0 @@
-function amg_gamma(data,lvl)
-		Aff = data.A{lvl}(data.F{lvl},data.F{lvl});
-		Afc = data.A{lvl}(data.F{lvl},data.C{lvl});
-		W = data.Wt{lvl}';
-		G = W'*(Aff*W + Afc) + Afc'*(W + Aff\Afc);
-		G = pinv(full(data.A{lvl+1}))*G;
-		gamma = sqrt(max(eig(G)))
-end
--- tools/amg_matlab/sym_sparsify.m
+++
@@ -1,4 +0,0 @@
-function S = sym_sparsify(A,tol)
-  S = sym_sparsify_U(sparse(A),tol);
-	S = S + S' + speye(length(A(:,1)));
-end
