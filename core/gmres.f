Nek5000/Nek5000

commit 00f0b94e02967a67b52b92fe9d5bd653f8d03bbe

Fixed errors in OpenACC MPI collective calls (#311)
* WIP: Corrected usage of glsc3_acc (not tested!)

* Fixed bug in glsum_acc

core/gmres.f
@@ -332,7 +332,7 @@ c     GMRES iteration.
 
       common /cgmres1/ y(lgmres)
       common /ctmp0/   wk1(lgmres),wk2(lgmres)
-      real alpha, l, temp, temp_ptr(1)
+      real alpha, l, temp, temp_ptr1(1), temp_ptr2(1)
       integer outer
 
       logical iflag,if_hyb
@@ -400,24 +400,31 @@ c           call copy(r,res,n)
          endif
 
 #ifdef _OPENACC
+
 c        ROR: 2017-06-03: I inlined glsc3_acc because I couldn't figure
 c        out how to call glsc3_acc from inside a kernel.  I kept getting
 c        the error: "Unsupported nested compute construct in compute
 c        construct or acc routine"
 
-!$ACC KERNELS PRESENT(r_gmres,wt) 
-         temp = 0.0
+!$ACC DATA CREATE(temp_ptr1,temp_ptr2)
+
+!$ACC KERNELS PRESENT(r_gmres,wt)
+         temp_ptr1(1) = 0.0
          do  k=1,n
-           temp = temp + r_gmres(k)*r_gmres(k)*wt(k)
+           temp_ptr1(1) = temp_ptr1(1) + r_gmres(k)*r_gmres(k)*wt(k)
          enddo
-         temp = sqrt(temp)
 !$ACC END KERNELS
 
-      call gop_acc(temp,temp_ptr,'+  ',1)
+      call gop_acc(temp_ptr1,temp_ptr2,'+  ',1)
 
-!$ACC KERNELS PRESENT(gamma_gmres)
-      gamma_gmres(1) = temp
+!$ACC KERNELS
+      gamma_gmres(1) = sqrt(temp_ptr1(1))
 !$ACC END KERNELS
+
+!$ACC END DATA
+
+!$ACC UPDATE HOST(gamma_gmres(1))
+      temp = gamma_gmres(1)
 
 #else
          gamma_gmres(1) = sqrt(glsc3(r_gmres,r_gmres,wt,n)) ! gamma  = \/ (r,r)

core/math.f
@@ -1099,21 +1099,22 @@ C
 C     Perform inner-product in double precision
 C
       real a(n),b(n),mult(n)
-      real tmp,work(1)
+      real tmp(1),work(1)
 
-      tmp = 0.0
+!$ACC DATA CREATE(work) COPYOUT(tmp) PRESENT(a,b,mult)
 
-!$ACC KERNELS PRESENT(a,b,mult)
+!$ACC KERNELS
+      tmp(1) = 0.0
       do  i=1,n
-         tmp = tmp + a(i)*b(i)*mult(i)
+         tmp(1) = tmp(1) + a(i)*b(i)*mult(i)
       enddo
 !$ACC END KERNELS
 
-!$ACC ENTER DATA CREATE(work)
       call gop_acc(tmp,work,'+  ',1)
-!$ACC EXIT DATA DELETE(work)
 
-      glsc3_acc = tmp
+!$ACC END DATA
+
+      glsc3_acc = tmp(1)
       return
       end
 
@@ -2006,14 +2007,17 @@ c-----------------------------------------------------------------------
       function glsum_acc (x,n)
       dimension x(n)
       dimension tmp(1),work(1)
-      tsum = 0.
-!$ACC KERNELS PRESENT(x)
+
+!$ACC DATA COPYOUT(tmp) CREATE(work) PRESENT(x)
+!$ACC KERNELS 
+      tmp(1) = 0.
       do i=1,n
-         tsum = tsum+x(i)
+         tmp(1) = tmp(1)+x(i)
       enddo
 !$ACC END KERNELS
-      tmp(1)=tsum
       call gop_acc(tmp,work,'+  ',1)
+!$ACC END DATA
+
       glsum = tmp(1)
       return
       END

commit 389b4d03a5487181988fbcf17b35614c0331f9dd

Fixed broken reduction kernels from 00f0b94 (#315)
* Fixed broken reduction kernels 00f0b94

* Fixed variable declarations for gfortran

core/gmres.f
@@ -409,10 +409,11 @@ c        construct or acc routine"
 !$ACC DATA CREATE(temp_ptr1,temp_ptr2)
 
 !$ACC KERNELS PRESENT(r_gmres,wt)
-         temp_ptr1(1) = 0.0
+         temp = 0.0
          do  k=1,n
-           temp_ptr1(1) = temp_ptr1(1) + r_gmres(k)*r_gmres(k)*wt(k)
+           temp = temp + r_gmres(k)*r_gmres(k)*wt(k)
          enddo
+         temp_ptr1(1) = temp
 !$ACC END KERNELS
 
       call gop_acc(temp_ptr1,temp_ptr2,'+  ',1)

core/math.f
@@ -1099,22 +1099,23 @@ C
 C     Perform inner-product in double precision
 C
       real a(n),b(n),mult(n)
-      real tmp(1),work(1)
+      real tmp,tmp_ptr(1),work(1)
 
-!$ACC DATA CREATE(work) COPYOUT(tmp) PRESENT(a,b,mult)
+!$ACC DATA CREATE(work) COPYOUT(tmp_ptr) PRESENT(a,b,mult)
 
 !$ACC KERNELS
-      tmp(1) = 0.0
+      tmp = 0.0
       do  i=1,n
-         tmp(1) = tmp(1) + a(i)*b(i)*mult(i)
+         tmp = tmp + a(i)*b(i)*mult(i)
       enddo
+      tmp_ptr(1) = tmp
 !$ACC END KERNELS
 
-      call gop_acc(tmp,work,'+  ',1)
+      call gop_acc(tmp_ptr,work,'+  ',1)
 
 !$ACC END DATA
 
-      glsc3_acc = tmp(1)
+      glsc3_acc = tmp_ptr(1)
       return
       end
 
@@ -2005,19 +2006,21 @@ c-----------------------------------------------------------------------
       END
 c-----------------------------------------------------------------------
       function glsum_acc (x,n)
-      dimension x(n)
-      dimension tmp(1),work(1)
+      real x(n)
+      integer n
+      real tmp, tmp_ptr(1),work(1)
 
-!$ACC DATA COPYOUT(tmp) CREATE(work) PRESENT(x)
+!$ACC DATA COPYOUT(tmp_ptr) CREATE(work) PRESENT(x)
 !$ACC KERNELS 
-      tmp(1) = 0.
+      tmp = 0.
       do i=1,n
-         tmp(1) = tmp(1)+x(i)
+         tmp = tmp+x(i)
       enddo
+      tmp_ptr(1) = tmp
 !$ACC END KERNELS
-      call gop_acc(tmp,work,'+  ',1)
+      call gop_acc(tmp_ptr,work,'+  ',1)
 !$ACC END DATA
 
-      glsum = tmp(1)
+      glsum = tmp_ptr(1)
       return
       END