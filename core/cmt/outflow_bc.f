a2739556133208b59382f107495431f842991740
hackljf <jason.hackl@ufl.edu>
2017-08-31 13:04:13 -0400

    OK NOW THE BC are fixed

--- core/cmt/outflow_bc.f
+++ core/cmt/outflow_bc.f
@@ -1,14 +1,13 @@
 C> @file outflow_bc.f Dirichlet states for outflow boundary conditions
-      subroutine outflow(nvar,f,e,facew,wbc,flux) ! don't really need nvar anymore
+      subroutine outflow(nvar,f,e,facew,wbc) ! don't really need nvar anymore
       INCLUDE 'SIZE'
       INCLUDE 'INPUT'
 
       integer  nvar,f,e
       real facew(nx1,nz1,2*ldim,nelt,nvar)
       real wbc(nx1,nz1,2*ldim,nelt,nvar)
-      real flux(nx1*nz1,2*ldim,nelt,*)
 
-      call outflow_rflu(nvar,f,e,facew,wbc,flux)
+      call outflow_rflu(nvar,f,e,facew,wbc)
 
       return
       end
