ce34d31f9b4eaefee1988e9d04e5e039a5ad7254
Adam Peplinski <adam@mech.kth.se>
2017-09-27 13:43:37 +0200

    adding test to unittest

--- short_tests/NekTests.py
+++ short_tests/NekTests.py
@@ -1171,6 +1171,66 @@ class LinCav_Adj(NekTestCase):
 
     def tearDown(self):
         self.move_logs()
+
+####################################################################
+#  io_test; io_test.par
+#  For serial run tests with multiple files are omitted
+####################################################################
+class IO_Test(NekTestCase):
+    example_subdir = 'io_test'
+    case_name = 'io_test'
+
+    def setUp(self):
+        self.size_params = dict(
+            ldim      = '3',
+            lx1       = '6',
+            lxd       = '9',
+            lx2       = 'lx1-2',
+            lelg      = '100',
+            ldimt     = '3',
+        )
+
+        self.build_tools(['genmap'])
+        self.run_genmap()
+
+    @pn_pn_2_serial
+    def test_PnPn2_Serial(self):
+        self.size_params['lx2'] = 'lx1-2'
+        self.config_size()
+        self.build_nek(usr_file='io_test')
+        self.run_nek(step_limit=None)
+
+        phrase = self.get_phrase_from_log('ERROR: field')
+        self.assertIsNullDelayed(phrase, label='ERROR: field')
+        
+        phrase = self.get_phrase_from_log('ERROR: param')
+        self.assertIsNullDelayed(phrase, label='ERROR: param')
+
+        phrase = self.get_phrase_from_log('All I/O test finished')
+        self.assertIsNotNullDelayed(phrase, label='All I/O test finished')     
+
+        self.assertDelayedFailures()
+        
+    @pn_pn_2_parallel
+    def test_PnPn2_Parallel(self):
+        self.size_params['lx2'] = 'lx1-2'
+        self.config_size()
+        self.build_nek(usr_file='io_test')
+        self.run_nek(step_limit=None)  
+
+        phrase = self.get_phrase_from_log('ERROR: field')
+        self.assertIsNullDelayed(phrase, label='ERROR: field')
+        
+        phrase = self.get_phrase_from_log('ERROR: param')
+        self.assertIsNullDelayed(phrase, label='ERROR: param')
+
+        phrase = self.get_phrase_from_log('All I/O test finished')
+        self.assertIsNotNullDelayed(phrase, label='All I/O test finished')
+
+        self.assertDelayedFailures()
+
+    def tearDown(self):
+        self.move_logs()
         
 ###############################################################
 if __name__ == '__main__':
