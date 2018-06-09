afc20a56b40832eb4d74a802af7e53857213bde8
Paul Fischer <fischer@v8.mcs.anl.gov>
2018-06-09 16:19:51 -0500

    fix large array f at line 1020

--- tools/postnek/postnek6.f
+++ tools/postnek/postnek6.f
@@ -1025,7 +1025,7 @@ c-----------------------------------------------------------------------
       subroutine mapz(f,regdir)
 #     include "basics.inc"
 c
-      real f(nxm*nym*nzm*nelm)
+      real f(1)
       real gll_pts(nxm),gll_wts(nxm),i(nxm*nxm),it(nxm*nxm)
       real w(nxm*nxm*nxm)
       character*1 regdir
