11f96dbbc1401b00d3ef1353f170dce42b878c02
Thilina Rathnayake <thilinarmtb@gmail.com>
2018-06-12 14:24:37 -0500

    Expose get/set_bytesw_write to Fortran

--- core/byte.c
+++ core/byte.c
@@ -30,13 +30,15 @@
 
 #endif
 
-#define byte_reverse  FORTRAN_NAME(byte_reverse,  BYTE_REVERSE)
-#define byte_reverse8 FORTRAN_NAME(byte_reverse8, BYTE_REVERSE8)
-#define byte_open     FORTRAN_NAME(byte_open,     BYTE_OPEN   )
-#define byte_close    FORTRAN_NAME(byte_close,    BYTE_CLOSE  )
-#define byte_rewind   FORTRAN_NAME(byte_rewind,   BYTE_REWIND )
-#define byte_read     FORTRAN_NAME(byte_read,     BYTE_READ   )
-#define byte_write    FORTRAN_NAME(byte_write,    BYTE_WRITE  )
+#define byte_reverse     FORTRAN_NAME(byte_reverse,     BYTE_REVERSE    )
+#define byte_reverse8    FORTRAN_NAME(byte_reverse8,    BYTE_REVERSE8   )
+#define byte_open        FORTRAN_NAME(byte_open,        BYTE_OPEN       )
+#define byte_close       FORTRAN_NAME(byte_close,       BYTE_CLOSE      )
+#define byte_rewind      FORTRAN_NAME(byte_rewind,      BYTE_REWIND     )
+#define byte_read        FORTRAN_NAME(byte_read,        BYTE_READ       )
+#define byte_write       FORTRAN_NAME(byte_write,       BYTE_WRITE      )
+#define get_bytesw_write FORTRAN_NAME(get_bytesw_write, GET_BYTESW_WRITE)
+#define set_bytesw_write FORTRAN_NAME(set_bytesw_write, SET_BYTESW_WRITE)
 
 #define READ     1
 #define WRITE    2
