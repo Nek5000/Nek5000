3b8326754c914f12b944416f3f53889ab94d8e60
Kento Kaneko <kaneko2@illinois.edu>
2020-05-19 09:51:04 -0500

    more precisely set ifprojfld(nfld)

--- core/navier5.f
+++ core/navier5.f
@@ -3389,15 +3389,16 @@ c     enddo
       param(7) = param(1)  ! rhoCP   = rho
       param(8) = param(2)  ! conduct = dyn. visc
 
-      ifheat       = .true.
-      ifadvc(nfld) = .true.
-      iftmsh(nfld) = .true.
-      ifvarp(nfld) = ifvarp(nfield)
-      ifdeal(nfld) = ifdeal(nfield)
+      ifheat          = .true.
+      ifadvc(nfld)    = .true.
+      iftmsh(nfld)    = .true.
+      ifvarp(nfld)    = ifvarp(nfield)
+      ifdeal(nfld)    = ifdeal(nfield)
+      ifprojfld(nfld) = ldimt_proj.ge.(nfld-1).and.ifprojfld(nfield)
+
       if (nfld.eq.2) ifto = .true.
       if (nfld.gt.2) ifpsco(nfld-2) = .true.
       if (nfld.gt.2) npscal = npscal+1
-      if (ldimt_proj.ge.(nfld-1)) ifprojfld(nfld)=ifprojfld(nfield)
 
       ifldmhd = npscal + 3
 
