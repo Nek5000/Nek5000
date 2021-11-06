6df657d4f4a67fe6880ffdd93cd039e6b7dc36a7
Stefan <>
2021-11-06 17:16:36 +0100

    print number of curves sides

--- core/reader_re2.f
+++ core/reader_re2.f
@@ -202,7 +202,7 @@ c-----------------------------------------------------------------------
       re2off_b = re2off_b + nrg*4*lrs4
 
       if (.not.ifread) return
-      if(nio.eq.0) write(6,*) 'reading curved sides '
+      if(nio.eq.0) write(6,*) 'reading curved sides ', nrg
 
       nwds4r = nr*lrs4
       call byte_set_view(lre2off_b,fh_re2)
