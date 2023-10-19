6214afe9cabedb6e65d2f0445597a9db14ba0c09
Nadish Saini <nsaini@anl.gov>
2023-10-19 00:27:31 -0500

    WIP

---
+++ core/experimental/lshmholtz.f
@@ -0,0 +1,386 @@
+c-----------------------------------------------------------------------
+      subroutine hsolve_cls(name,u,r,h1,h2,vmk,vml,
+     $                 imsh,tol,maxit,isd
+     $                 ,approx,napprox,bi)
+c
+c
+      include 'SIZE'
+      include 'TOTAL'
+      include 'CTIMER'
+c
+      CHARACTER*4    NAME
+      REAL           U    (LX1,LY1,LZ1,1)
+      REAL           R    (LX1,LY1,LZ1,1)
+      REAL           H1   (LX1,LY1,LZ1,1)
+      REAL           H2   (LX1,LY1,LZ1,1)
+      REAL           vmk  (LX1,LY1,LZ1,1)
+      REAL           vml  (LX1,LY1,LZ1,1)
+      REAL           bi   (LX1,LY1,LZ1,1)
+      REAL           approx (1)
+      integer        napprox(1)
+      common /ctmp2/ w1   (lx1,ly1,lz1,lelt)
+      common /ctmp3/ w2   (2+2*mxprev)
+
+      logical ifstdh
+      character*4  cname
+      character*6  name6
+
+      logical ifwt,ifvec
+
+      call chcopy(cname,name,4)
+      call capit (cname,4)
+
+      ifstdh = .true. ! default is no projection
+      if (ifprojfld(ifield)) then
+         ifstdh = .false.
+      endif
+
+      p945 = param(94)
+      if (cname.eq.'PRES') then
+         ifstdh = .false.
+         p945 = param(95)
+      endif
+
+      if (ifield.gt.ldimt_proj+1) ifstdh = .true.
+      if (param(93).eq.0)         ifstdh = .true.
+      if (p945.eq.0)              ifstdh = .true.
+      if (istep.lt.p945)          ifstdh = .true.
+
+      if (ifstdh) then
+         call hmholtz_cls(name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd)
+      else
+
+         n = lx1*ly1*lz1*nelfld(ifield)
+
+         call col2   (r,vmk,n)
+         call dssum  (r,lx1,ly1,lz1)
+
+         call blank (name6,6)
+         call chcopy(name6,name,4)
+         ifwt  = .true.
+         ifvec = .false.
+
+         call project1
+     $       (r,n,approx,napprox,h1,h2,vmk,vml,ifwt,ifvec,name6)
+
+         call hmhzpf_cls (name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd,bi)
+
+         call project2
+     $       (u,n,approx,napprox,h1,h2,vmk,vml,ifwt,ifvec,name6)
+
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine hmholtz_cls(name,u,rhs,h1,h2,mask,mult,
+     $  imsh,tli,maxit,isd)
+      include 'SIZE'
+      include 'TOTAL'
+      include 'FDMH1'
+      include 'CTIMER'
+
+      CHARACTER      NAME*4
+      REAL           U    (LX1,LY1,LZ1,1)
+      REAL           RHS  (LX1,LY1,LZ1,1)
+      REAL           H1   (LX1,LY1,LZ1,1)
+      REAL           H2   (LX1,LY1,LZ1,1)
+      REAL           MASK (LX1,LY1,LZ1,1)
+      REAL           MULT (LX1,LY1,LZ1,1)
+
+      logical iffdm
+      character*3 nam3
+
+      tol = abs(tli)
+
+      iffdm = .false.
+      if (ifsplit) iffdm = .true.
+      if (icalld.eq.0.and.iffdm) call set_fdm_prec_h1A
+      icalld = icalld+1
+
+#ifdef TIMER
+      if (name.ne.'PRES') then
+        nhmhz = nhmhz + 1
+        etime1 = dnekclock()
+      endif
+#endif
+
+      ntot = lx1*ly1*lz1*nelfld(ifield)
+      if (imsh.eq.1) ntot = lx1*ly1*lz1*nelv
+      if (imsh.eq.2) ntot = lx1*ly1*lz1*nelt
+
+c
+      call chcopy(nam3,name,3)
+c
+                          kfldfdm = -1
+c     if (nam3.eq.'TEM' ) kfldfdm =  0
+c     if (name.eq.'TEM1') kfldfdm =  0  ! hardcode for temp only, for mpaul
+c     if (name.eq.'VELX') kfldfdm =  1
+c     if (name.eq.'VELY') kfldfdm =  2
+c     if (name.eq.'VELZ') kfldfdm =  3
+      if (name.eq.'PRES') kfldfdm =  ldim+1
+c     if (.not.iffdm) kfldfdm=-1
+C
+      call dssum   (rhs,lx1,ly1,lz1)
+      call col2    (rhs,mask,ntot)
+c      if (nio.eq.0.and.istep.le.10) 
+c     $    write(6,*) param(22),' p22 ',istep,imsh
+      if (param(22).eq.0.or.istep.le.10)
+     $    call chktcg1 (tol,rhs,h1,h2,mask,mult,imsh,isd)
+
+      if (tli.lt.0) tol=tli ! caller-specified relative tolerance
+
+      if (imsh.eq.1) call cggo
+     $   (u,rhs,h1,h2,mask,mult,imsh,tol,maxit,isd,binvm1,name)
+      if (imsh.eq.2) call cggo
+     $   (u,rhs,h1,h2,mask,mult,imsh,tol,maxit,isd,bintm1,name)
+
+#ifdef TIMER
+      if (name.ne.'PRES') thmhz=thmhz+(dnekclock()-etime1)
+#endif
+
+      return
+      END
+c-----------------------------------------------------------------------
+      subroutine hmhzpf_cls(name,u,r,h1,h2,mask,mult,imesh,
+     $   tli,maxit,isd,bi)
+      include 'SIZE'
+      include 'INPUT'
+      include 'MASS'
+      include 'FDMH1'
+      include 'CTIMER'
+c
+      CHARACTER*4    NAME
+      REAL           U    (LX1,LY1,LZ1,1)
+      REAL           R    (LX1,LY1,LZ1,1)
+      REAL           H1   (LX1,LY1,LZ1,1)
+      REAL           H2   (LX1,LY1,LZ1,1)
+      REAL           MASK (LX1,LY1,LZ1,1)
+      REAL           MULT (LX1,LY1,LZ1,1)
+      REAL           bi   (LX1,LY1,LZ1,1)
+      COMMON /CTMP0/ W1   (LX1,LY1,LZ1,LELT)
+     $ ,             W2   (LX1,LY1,LZ1,LELT)
+c
+      etime1=dnekclock()
+c
+      IF (IMESH.EQ.1) NTOT = lx1*ly1*lz1*NELV
+      IF (IMESH.EQ.2) NTOT = lx1*ly1*lz1*NELT
+c
+      tol = tli
+      if (param(22).ne.0) tol = abs(param(22))
+      CALL CHKTCG1 (TOL,R,H1,H2,MASK,MULT,IMESH,ISD)
+c
+c
+c     Set flags for overlapping Schwarz preconditioner (pff 11/12/98)
+c
+                          kfldfdm = -1
+c     if (name.eq.'TEMP') kfldfdm =  0
+c     if (name.eq.'VELX') kfldfdm =  1
+c     if (name.eq.'VELY') kfldfdm =  2
+c     if (name.eq.'VELZ') kfldfdm =  3
+      if (name.eq.'PRES') kfldfdm =  ldim+1
+
+      if (ifdg) then
+         call cggo_dg (u,r,h1,h2,bi,mask,name,tol,maxit)
+      else
+         call cggo
+     $      (u,r,h1,h2,mask,mult,imesh,tol,maxit,isd,bi,name)
+      endif
+      thmhz=thmhz+(dnekclock()-etime1)
+c
+c
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine cgg(x,f,h1,h2,mask,mult,imsh,tin,maxit,isd,binv,name)
+      include 'SIZE'
+      include 'TOTAL'
+      include 'FDMH1'
+ 
+      COMMON  /CPRINT/ IFPRINT, IFHZPC
+      LOGICAL          IFPRINT, IFHZPC
+ 
+      common /fastmd/ ifdfrm(lelt), iffast(lelt), ifh2, ifsolv
+      logical ifdfrm, iffast, ifh2, ifsolv
+
+      logical ifmcor,ifprint_hmh
+ 
+      real x(1),f(1),h1(1),h2(1),mask(1),mult(1),binv(1)
+      parameter        (lg=lx1*ly1*lz1*lelt)
+      COMMON /SCRCG/ d (lg) , scalar(2)
+      common /SCRMG/ r (lg) , w (lg) , p (lg) , z (lg)
+c
+      parameter (maxcg=900)
+      common /tdarray/ diagt(maxcg),upper(maxcg)
+      common /iterhm/ niterhm
+      character*4 name
+
+      if (ifsplit.and.name.eq.'PRES') then
+         if (param(42).eq.0) then
+           n = lx1*ly1*lz1*nelv
+           call copy      (x,f,n)
+           iter = maxit
+           call hmh_gmres (x,h1,h2,mult,iter)
+           niterhm = iter
+           return
+         elseif(param(42).eq.2) then 
+           n = lx1*ly1*lz1*nelv
+           call copy       (x,f,n)
+           iter = maxit
+           call hmh_flex_cg(x,h1,h2,mult,iter)
+           niterhm = iter
+           return
+         endif
+      endif
+
+      call rzero(diagt,maxcg)
+      call rzero(upper,maxcg)
+      rho = 0.00
+
+      NXYZ   = lx1*ly1*lz1
+      NEL    = NELV
+      VOL    = VOLVM1
+      IF (IMSH.EQ.2) NEL=NELT
+      IF (IMSH.EQ.2) VOL=VOLTM1
+      n      = NEL*NXYZ
+
+      tol=abs(tin)
+
+      if (restol(ifield).ne.0) tol=restol(ifield)
+      if (name.eq.'PRES'.and.param(21).ne.0) tol=abs(param(21))
+
+      if (tin.lt.0) tol=abs(tin)
+      niter = min(maxit,maxcg)
+
+      if (.not.ifsolv) then
+         call setfast(h1,h2,imsh)
+         ifsolv = .true.
+      endif
+
+      if (kfldfdm.lt.0) then
+         call setprec(D,h1,h2,imsh,isd)
+      elseif(param(100).ne.2) then
+         call set_fdm_prec_h1b(d,h1,h2,nel)
+      endif
+
+      call copy (r,f,n)
+      call rzero(x,n)
+      call rzero(p,n)
+
+      fmax = glamax(f,n)
+      if (fmax.eq.0.0) return
+
+
+      ifmcor = .false.
+      h2max = glmax(h2  ,n)
+      skmin = glmin(mask,n)
+      if (skmin.gt.0.and.h2max.eq.0) ifmcor = .true.
+
+      if (name.eq.'PRES') then
+        continue
+      elseif (ifmcor) then
+         smean = -1./glsum(bm1,n) ! Modified 5/4/12 pff
+         rmean = smean*glsc2(r,mult,n)
+         call copy(x,bm1,n)
+         call dssum(x,lx1,ly1,lz1)
+         call add2s2(r,x,rmean,n)
+         call rzero(x,n)
+      endif
+C
+      krylov = 0
+      rtz1=1.0
+      niterhm = 0
+
+      do iter=1,niter
+        if (kfldfdm.lt.0) then  ! Jacobi Preconditioner
+          call col3(z,r,d,n)
+        else                                       ! Schwarz Preconditioner
+          if (name.eq.'PRES'.and.param(100).eq.2) then
+            call h1_overlap_2(z,r,mask)
+            call crs_solve_h1 (w,r)  ! Currently, crs grd only for P
+            call add2         (z,w,n)
+          else   
+            call fdm_h1(z,r,d,mask,mult,nel,ktype(1,1,kfldfdm),w)
+            if (name.eq.'PRES') then 
+              call crs_solve_h1 (w,r)  ! Currently, crs grd only for P
+              call add2         (z,w,n)
+            endif
+          endif
+        endif
+        if (name.eq.'PRES') then
+          call ortho (z)
+        elseif (ifmcor) then
+          rmean = smean*glsc2(z,bm1,n)
+          call cadd(z,rmean,n)
+        endif
+
+        rtz2=rtz1
+        scalar(1)=vlsc3 (z,r,mult,n)
+        if(param(18).eq.1) then
+          scalar(2)=vlsc3(r,r,mult,n)
+        else 
+          scalar(2)=vlsc32(r,mult,binv,n)
+        endif
+        call gop(scalar,w,'+  ',2)
+        rtz1=scalar(1)
+        rbn2=sqrt(scalar(2)/vol)
+        if (iter.eq.1) rbn0 = rbn2
+        if (param(22).lt.0) tol=abs(param(22))*rbn0
+        if (tin.lt.0)       tol=abs(tin)*rbn0
+
+         ifprint_hmh = .false.
+         if (nio.eq.0.and.ifprint.and.param(74).ne.0) ifprint_hmh=.true.
+         if (nio.eq.0.and.istep.eq.1)                 ifprint_hmh=.true.
+
+         if (ifprint_hmh)
+     &      write(6,3002) istep,'  Hmholtz ' // name,
+     &                    iter,rbn2,h1(1),tol,h2(1),ifmcor
+
+
+         iter_max = param(150)
+         if (name.eq.'PRES') iter_max = param(151)
+         if (iter.gt.iter_max) then
+            NITER = ITER-1
+            if (nio.eq.0)
+     &         write(6,3000) istep,'  Hmholtz ' // name,
+     &                       niter,rbn2,rbn0,tol
+            exit
+         ENDIF
+c
+         beta = rtz1/rtz2
+         if (iter.eq.1) beta=0.0
+         call add2s1 (p,z,beta,n)
+         call axhelm_cls (w,p,h1,h2,imsh,isd)
+         call dssum  (w,lx1,ly1,lz1)
+         call col2   (w,mask,n)
+c
+         rho0 = rho
+         rho  = glsc3(w,p,mult,n)
+         alpha=rtz1/rho
+         alphm=-alpha
+         call add2s2(x,p ,alpha,n)
+         call add2s2(r,w ,alphm,n)
+c
+c        Generate tridiagonal matrix for Lanczos scheme
+         if (iter.eq.1) then
+            krylov = krylov+1
+            diagt(iter) = rho/rtz1
+         elseif (iter.le.maxcg) then
+            krylov = krylov+1
+            diagt(iter)    = (beta**2 * rho0 + rho ) / rtz1
+            upper(iter-1)  = -beta * rho0 / sqrt(rtz2 * rtz1)
+         endif
+      enddo
+      niter = iter-1
+c
+      if (nio.eq.0) write (6,3001) istep, '  Error Hmholtz ' // name,
+     &                             niter,rbn2,rbn0,tol
+
+
+3000  format(i11,a,1x,I7,1p4E13.4)
+3001  format(i11,a,1x,I7,1p4E13.4)
+3002  format(i11,a,1x,I7,1p4E13.4,l4)
+      niterhm = niter
+      ifsolv = .false.
+      return
+      end
---
+++ core/experimental/lvlSet.f
@@ -0,0 +1,475 @@
+C----------------------------------------------------------------------     
+      include "experimental/lshmholtz.f"
+C----------------------------------------------------------------------     
+      subroutine ls_init(nsteps_in,eps_in,
+     $                   ifld_cls_in,ifld_clsr_in,
+     $                   ifld_tls_in,ifld_tlsr_in)
+      implicit none
+      include 'SIZE'
+      include 'LVLSET'
+
+      real eps_in
+      integer nsteps_in
+      integer ifld_cls_in, ifld_tls_in
+      integer ifld_clsr_in, ifld_tlsr_in
+      
+      ! multiple of element length
+      eps_cls = eps_in
+      
+      nsteps_cls = nsteps_in
+
+      ifld_cls = ifld_cls_in
+      ifld_clsr = ifld_clsr_in
+      ifld_tls = ifld_tls_in
+      ifld_tlsr = ifld_tlsr_in
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine ls_drive
+      implicit none
+      include 'SIZE'
+      include 'TSTEP'
+      include 'LVLSET'
+
+      integer istep_save, ifld_save
+      real dt_save, time_save
+      integer i
+
+      !replace internal istep
+      istep_save = ISTEP
+      dt_save = dt
+      time_save = time
+      ifld_save = ifield
+
+      ISTEP = 0
+      dt = dt_cls
+      time = 0.0
+      ifield = ifld_clsr
+
+      !Convert normal vector to rst space
+      !Note that normals do not change over re-dist steps
+      call vector_to_rst(clsnx,clsny,clsnz,
+     $                   clsnr,clsns,clsnt)
+
+      do i=1,nsteps_cls
+        istep = istep + 1
+        call ls_advance
+      enddo
+
+      !replace back
+      ISTEP = istep_save
+      dt = dt_save
+      time = time_save
+      ifield = ifld_save
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine ls_advance
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+      include 'LVLSET'
+
+      integer igeom
+
+      call nekgsync()
+
+      !reuse vxd,vyd,vzd 
+      call set_convect_cons(vxd,vyd,vzd,clsnx,clsny,clsnz) 
+
+      call settime_cls
+
+      do igeom = 1,ngeom
+        call unorm
+        !diffusion array must be filled out here - pending
+        call settolt
+        call cdcls(igeom)
+      enddo
+
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine settime_cls
+      implicit none
+      include 'SIZE'
+      include 'INPUT'
+      include 'TSTEP'
+
+      integer irst, ilag
+
+      irst = param(46)
+
+      do ILAG=10,2,-1
+        DTLAG(ILAG) = DTLAG(ILAG-1)
+      enddo
+
+      CALL setdt_cls
+      DTLAG(1) = DT
+      IF (ISTEP.EQ.1 .and. irst.le.0) DTLAG(2) = DT
+
+      TIMEF    = TIME
+      TIME     = TIME+DT
+
+      CALL SETORDBD
+      if (irst.gt.0) nbd = nbdinp
+      CALL RZERO (BD,10)
+      CALL SETBD (BD,DTLAG,NBD)
+      if (PARAM(27).lt.0) then
+        NAB = NBDINP
+      else
+        NAB = 3
+      endif
+      IF (ISTEP.lt.NAB.and.irst.le.0) NAB = ISTEP
+      CALL RZERO   (AB,10)
+      CALL SETABBD (AB,DTLAG,NAB,NBD)
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine setdt_cls
+      implicit none
+      include 'SIZE'
+      include 'LVLSET'
+
+      real umax
+
+      call compute_cfl(umax,clsnx,clsny,clsnz,1)
+
+      ! worry about adjust dt based on CFL later
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine cdcls(igeom)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+      include 'LVLSET'
+      include 'ORTHOT'
+
+      common /lsscratch/ ta(lx1,ly1,lz1,lelt),
+     $                   tb(lx1,ly1,lz1,lelt) 
+      real ta,tb
+
+      COMMON /SCRVH/ H1(LX1,LY1,LZ1,LELT),
+     $               H2(lx1,ly1,lz1,lelt) 
+
+      real h1,h2
+
+      integer igeom,n,iter
+      logical ifconv
+      integer ifld1
+
+      n = lx1*ly1*lz1*nelv
+
+      ifld1 = ifield-1
+
+      if(igeom.eq.1)then
+        call makeq_cls
+        call lagscal
+      else
+        write(name4t,'(A4)')"CLSR"
+
+        call sethlm_cls(h1,h2)
+
+        do iter=1,nmxnl
+          call axhelm_cls(ta,t(1,1,1,1,ifield-1),h1,h2) 
+          call sub3(tb,bq(1,1,1,1,ifield-1),ta,n)
+
+          call hsolve_cls(name4t,ta,tb,h1,h2,
+     $                tmask(1,1,1,1,ifield-1),
+     $                tmult(1,1,1,1,ifield-1),
+     $                imesh,tolht(ifield),nmxt(ifield-1),1,
+     $                approxt(1,0,ifld1),napproxt(1,ifld1),binvm1)
+
+          call add2(t(1,1,1,1,ifield-1),ta,n)
+          call cvgnlps (ifconv)
+          if (ifconv) exit
+        enddo
+      endif
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine axhelm_cls(au,u,helm1,helm2)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+      include 'LVLSET'
+
+      real au(lx1,ly1,lz1,1)
+      real u(lx1,ly1,lz1,1)
+      real helm1(lx1,ly1,lz1,1)
+      real helm2(lx1,ly1,lz1,1)
+
+      COMMON /CTMP1/ DUDR  (LX1,LY1,LZ1)
+     $  ,             DUDS  (LX1,LY1,LZ1)
+     $  ,             DUDT  (LX1,LY1,LZ1)
+     $  ,             TMP1  (LX1,LY1,LZ1)
+     $  ,             TMP2  (LX1,LY1,LZ1)
+     $  ,             TMP3  (LX1,LY1,LZ1)
+      real dudr,duds,dudt,tmp1,tmp2,tmp3
+
+      real tm1(lx1,ly1,lz1)
+      real tm2(lx1,ly1,lz1)
+      real tm3(lx1,ly1,lz1)
+      equivalence (dudr,tm1),(duds,tm2),(dudt,tm3)
+
+      COMMON /FASTMD/ IFDFRM(LELT), IFFAST(LELT), IFH2, IFSOLV
+      LOGICAL IFDFRM, IFFAST, IFH2, IFSOLV
+
+      integer ntot,nxy,nyz,nxz,nxyz
+      integer e,iz
+
+      common /ltmp/ ur(lx1,ly1,lz1),
+     $              us(lx1,ly1,lz1),
+     $              ut(lx1,ly1,lz1)
+      real ur,us,ut
+
+      NXY=lx1*ly1
+      NYZ=ly1*lz1
+      NXZ=lx1*lz1
+      NXYZ=lx1*ly1*lz1
+      NTOT=NXYZ*NELV
+
+      call rzero(au,ntot)
+
+      do e = 1,nelv
+        if(.not.if3d)then
+          call col3(ur,clsnr(1,1,1,e),u(1,1,1,e),NXYZ)
+          call col3(us,clsns(1,1,1,e),u(1,1,1,e),NXYZ)
+          call mxm  (dxm1,lx1,ur,lx1,dudr,nyz)
+          call mxm  (us,lx1,dytm1,ly1,duds,ly1)
+          call col3 (tmp1,dudr,g1m1(1,1,1,e),nxyz)
+          call col3 (tmp2,duds,g2m1(1,1,1,e),nxyz)
+          if (ifdfrm(e)) then
+            call addcol3 (tmp1,duds,g4m1(1,1,1,e),nxyz)
+            call addcol3 (tmp2,dudr,g4m1(1,1,1,e),nxyz)
+          endif
+          call col2 (tmp1,helm1(1,1,1,e),nxyz)
+          call col2 (tmp2,helm1(1,1,1,e),nxyz)
+          call mxm  (dxtm1,lx1,tmp1,lx1,tm1,nyz)
+          call mxm  (tmp2,lx1,dym1,ly1,tm2,ly1)
+          call col2(tm1,clsnr(1,1,1,e),NXYZ)
+          call col2(tm2,clsns(1,1,1,e),NXYZ)
+          call add2 (au(1,1,1,e),tm1,nxyz)
+          call add2 (au(1,1,1,e),tm2,nxyz)
+        else
+          call col3(ur,clsnr(1,1,1,e),u(1,1,1,e),NXYZ)
+          call col3(us,clsns(1,1,1,e),u(1,1,1,e),NXYZ)
+          call col3(ut,clsnt(1,1,1,e),u(1,1,1,e),NXYZ)
+          call mxm(dxm1,lx1,ur,lx1,dudr,nyz)
+          do iz=1,lz1
+            call mxm(us,lx1,dytm1,ly1,duds(1,1,iz),ly1)
+          enddo
+          call mxm     (ut,nxy,dztm1,lz1,dudt,lz1)
+          call col3    (tmp1,dudr,g1m1(1,1,1,e),nxyz)
+          call col3    (tmp2,duds,g2m1(1,1,1,e),nxyz)
+          call col3    (tmp3,dudt,g3m1(1,1,1,e),nxyz)
+          if (ifdfrm(e)) then
+            call addcol3 (tmp1,duds,g4m1(1,1,1,e),nxyz)
+            call addcol3 (tmp1,dudt,g5m1(1,1,1,e),nxyz)
+            call addcol3 (tmp2,dudr,g4m1(1,1,1,e),nxyz)
+            call addcol3 (tmp2,dudt,g6m1(1,1,1,e),nxyz)
+            call addcol3 (tmp3,dudr,g5m1(1,1,1,e),nxyz)
+            call addcol3 (tmp3,duds,g6m1(1,1,1,e),nxyz)
+          endif
+          call col2 (tmp1,helm1(1,1,1,e),nxyz)
+          call col2 (tmp2,helm1(1,1,1,e),nxyz)
+          call col2 (tmp3,helm1(1,1,1,e),nxyz)
+          call mxm  (dxtm1,lx1,tmp1,lx1,tm1,nyz)
+          do iz=1,lz1
+            call mxm(tmp2(1,1,iz),lx1,dym1,ly1,tm2(1,1,iz),ly1)
+          enddo
+          call mxm  (tmp3,nxy,dzm1,lz1,tm3,lz1)
+          call col2(tm1,clsnr(1,1,1,e),NXYZ)
+          call col2(tm2,clsns(1,1,1,e),NXYZ)
+          call col2(tm3,clsnt(1,1,1,e),NXYZ)
+          call add2 (au(1,1,1,e),tm1,nxyz)
+          call add2 (au(1,1,1,e),tm2,nxyz)
+          call add2 (au(1,1,1,e),tm3,nxyz)
+        endif
+      enddo
+
+
+      if(ifh2) call addcol4 (au,helm2,bm1,u,ntot)
+
+      !lets worry about axisymmetry later
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine sethlm_cls(h1,h2)
+      implicit none
+      include 'SIZE'
+
+      real h1(lx1,ly1,lz1,lelt)
+      real h2(lx1,ly1,lz1,lelt)
+      integer n
+
+      n = lx1*ly1*lz1*nelv
+
+      call rone (h1,n)
+
+      call rone (h2,n)
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine vector_to_rst(ux,uy,uz,ur,us,ut)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+
+      real ux(1),uy(1),uz(1)
+      real ur(1),us(1),ut(1)
+
+      integer i,ntot
+      real xr,yr,zr,xs,ys,zs,xt,yt,zt
+
+      integer icalld
+      save icalld
+      data icalld /0/
+
+      common /lvlset_x/ xrm1(lx1,ly1,lz1,lelt),
+     $                  yrm1(lx1,ly1,lz1,lelt),
+     $                  zrm1(lx1,ly1,lz1,lelt),
+     $                  xsm1(lx1,ly1,lz1,lelt),
+     $                  ysm1(lx1,ly1,lz1,lelt),
+     $                  zsm1(lx1,ly1,lz1,lelt),
+     $                  xtm1(lx1,ly1,lz1,lelt),
+     $                  ytm1(lx1,ly1,lz1,lelt),
+     $                  ztm1(lx1,ly1,lz1,lelt)
+      real xrm1,yrm1,zrm1 
+      real xsm1,ysm1,zsm1
+      real xtm1,ytm1,ztm1
+
+      if(icalld.eq.0)then
+        call XYZRST(XRM1,YRM1,ZRM1,
+     $               XSM1,YSM1,ZSM1,
+     $                XTM1,YTM1,ZTM1,ifaxis)
+        icalld = 1
+      endif
+      ntot = lx1*ly1*lz1*nelv
+
+      if(if3d)then
+        do i=1,ntot
+          xr = xrm1(i,1,1,1)
+          yr = yrm1(i,1,1,1)
+          zr = zrm1(i,1,1,1)
+          xs = xsm1(i,1,1,1)
+          ys = ysm1(i,1,1,1)
+          zs = zsm1(i,1,1,1)
+          xt = xtm1(i,1,1,1)
+          yt = ytm1(i,1,1,1)
+          zt = ztm1(i,1,1,1)
+          ur(i) = xr*ux(i) + yr*uy(i) + zr*uz(i)
+          us(i) = xs*ux(i) + ys*uy(i) + zs*uz(i)
+          ut(i) = xt*ux(i) + yt*uy(i) + zt*uz(i)
+        enddo
+      else
+        xr = xrm1(i,1,1,1)
+        yr = yrm1(i,1,1,1)
+        xs = xsm1(i,1,1,1)
+        ys = ysm1(i,1,1,1)
+        ur(i) = xr*ux(i) + yr*uy(i)
+        us(i) = xs*ux(i) + ys*uy(i)
+      endif
+
+      return
+      end
+C----------------------------------------------------------------------     
+      subroutine makeq_cls
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+      include 'LVLSET'
+
+      integer ntot
+
+      common /lsscratch/ ta(lx1,ly1,lz1,lelt),
+     $                   tb(lx1,ly1,lz1,lelt) 
+      real ta,tb
+
+      integer i
+
+      ntot = lx1*ly1*lz1*nelv
+
+      call rzero(bq(1,1,1,1,ifield-1),ntot)
+
+      !(1-psi)*psi
+      call copy(tb,t(1,1,1,1,ifield-1),ntot)
+      call cmult(tb,-1.0,ntot)
+      call cadd(tb,1.0,ntot)
+      call col2(tb,t(1,1,1,1,ifield-1),ntot)
+
+      !convop
+      call rzero(ta,ntot)
+      call convect_cons(ta,tb,.false.,clsnx,clsny,clsnz,.false.)
+      call invcol2(ta,bm1,ntot)
+
+      !convab
+      do i=1,ntot
+        bq(i,1,1,1,ifield-1) = -bm1(i,1,1,1)*ta(i,1,1,1)
+      enddo
+
+      call makeabq
+
+      call makebdq
+
+      return
+      end
+c---------------------------------------------------------------
+      real function deltael(ix,iy,iz,iel)
+      implicit none
+      include 'SIZE'
+      include 'TOTAL'
+
+      integer ix,iy,iz,iel
+
+      real dx(lx1,ly1,lz1,lelt)
+      save dx 
+
+      integer icalld
+      data icalld /0/
+      save icalld
+
+      integer nxyz,n,ie
+      real dd,dinv
+      real dxmin_e
+      real dxmax_e
+
+      nxyz = nx1*ny1*nz1
+      n    = nxyz*nelv
+
+      if (icalld.eq.0 .or. ifmvbd) then
+         dinv = 1./ldim
+         do ie = 1,nelv
+            dd = dxmax_e(ie)
+            call cfill(dx(1,1,1,ie),dd,nxyz) 
+         enddo
+         icalld = 1
+      endif
+
+      deltael = dx(ix,iy,iz,iel)
+
+      return
+      end 
+c---------------------------------------------------------------
+      real function heaviside(ix,iy,iz,iel,phi)
+      include 'SIZE'
+      include 'LVLSET'
+
+      integer ix,iy,iz,iel
+
+      real eps, deltael, phi
+
+      eps = deltael(ix,iy,iz,iel)*eps_cls
+      heaviside = 0.5*(tanh(phi/(2.0*eps))+1.0)
+
+      return
+      end
+c---------------------------------------------------------------
