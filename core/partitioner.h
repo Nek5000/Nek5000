db1c392c5dc8ae51bbc337dbb78bac16f32ee364
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-05-14 14:01:04 -0400

    Move graph implementation to partitioner-weak.c

--- core/partitioner.h
+++ core/partitioner.h
@@ -27,4 +27,19 @@ int Zoltan2_partMesh(int *part, long long *vl, unsigned nel, int nv,
 int parHIP_partMesh(int *part, long long *vl, int nel, int nv, MPI_Comm comm,
                     int verbose);
 
+typedef struct {
+  uint   num_vertices;
+  ulong *vertex_ids;
+  int   *neighbor_index;
+  ulong *neighbor_ids;
+  int   *neighbor_procs;
+  int   *neighbor_weights;
+} graph_t;
+
+graph_t *graph_create(long long *vl, int nelt, int nv, struct comm *c);
+
+void graph_print(graph_t *graph);
+
+void graph_destroy(graph_t **graph);
+
 #endif // __NEK5000_PARTITIONER_H__
